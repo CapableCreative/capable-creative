67ab444f27f658efe50ce52a00362bf35fb8a437
Stephen Fox <stephen@capablecreative.com>
2019-11-22 23:40:34 -0600

    Not finished, but better - SLF

--- inc/js/cc.js
+++ inc/js/cc.js
@@ -4,11 +4,19 @@
 $( document ).ready(function() {
 // Clicking Portfolio on main C+C page changes the main section to display the portfolio.
     $("#portTrigger").on("click",function(){
-        $("#defaultWrap").animate({opacity: "0.0"}, 150);
-        $("#defaultWrap").animate({height:'0px'}, 250);
-        $("#portfolioWrap").animate({height: '100vh'}, 250); 
-        $("#portfolioWrap").animate({flexBasis: '75%'}, 105);      
-        $("nav.mainNav").animate({width: '25%'}, 125);
+        $("#defaultWrap").animate({opacity: "0.0"}, 350);
+        $("#defaultWrap").animate({height:'0px'}, 350);
+        $("#portfolioWrap").animate({height: '100vh'}, 350); 
+        $("#portfolioWrap").animate({flexBasis: '75%'}, 205);      
+        $("nav.mainNav").animate({width: '25%'}, 225);
         $(".tagline").css({display: "none"});
+    });
+    $("#homeNav").on("click",function(){
+        $("#defaultWrap").animate({opacity: "1.0"}, 250);
+        $("#portfolioWrap").animate({height:'0px'}, 350);
+        $("#defaultWrap").animate({height: '100vh'}, 350); 
+        //$("#portfolioWrap").animate({flexBasis: '75%'}, 105);      
+        $("nav.mainNav").animate({width: '33%'}, 225);
+        $(".tagline").css({display: "block"});
     });
 });
